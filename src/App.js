aad7c5ce53de48998fc54ee7544891dfe0eccd3e
github username <github email address>
2018-11-19 14:55:14 -0500

    set up some Redux (reducer, store, and Provider). Established a few components (HomeContainer, LogContainer)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import HomeContainer from './Components/HomeContainer'
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <h1>T1DLog</h1>
+      <HomeContainer/>
+      </div>
     );
   }
 }
