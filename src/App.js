6298fc4660c570e9c2b294b894d92798f920728a
github username <github email address>
2018-11-19 17:04:47 -0500

    added some static semantic UI. hardcoded in data.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import HomeContainer from './Components/HomeContainer'
+import NavBar from './Components/NavBar'
+import TimerContainer from './Components/TimerContainer'
+import 'semantic-ui-css/semantic.min.css';
 
 import './App.css';
 
@@ -7,8 +10,15 @@ class App extends Component {
   render() {
     return (
       <div>
-      <h1>T1DLog</h1>
+
+      <NavBar/>
+
+      <div className="ui center aligned huge header">T1DLog</div>
+
+      <TimerContainer/>
+
       <HomeContainer/>
+
       </div>
     );
   }
