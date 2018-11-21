fc8baa8f3149e048b77b96577fe990abb405aac8
github username <github email address>
2018-11-21 12:33:25 -0500

    added some table columns. got some data in there

--- src/Components/LogEntry.js
+++ src/Components/LogEntry.js
@@ -8,10 +8,16 @@ class LogEntry extends Component{
     return(
 
       <tr>
-      <td data-label="date">{this.props.entryObj.entry_date}</td>
-      <td data-label="time-administered">{this.props.entryObj.entry_starting_time}</td>
-      <td data-label="time-remaining">tktktk</td>
+      <td data-label="date">{this.props.entryObj.formatted_date}</td>
+      <td data-label="start-time">{this.props.entryObj.formatted_start_time}</td>
+      <td data-label="end_time">tktktk</td>
+      
+      <td data-label="insulin_name">tktktk</td>
+      <td data-label="insulin_duration">tktktk</td>
+      <td data-label="time_left">tktktk</td>
+
       <td data-label="status">tktktk</td>
+
       </tr>
 
     )
