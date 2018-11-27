1aa7036c690825ca5a9b5db85693034455e797de
github username <github email address>
2018-11-26 23:22:17 -0500

    added some date logic not finished

--- src/Components/LogEntry.js
+++ src/Components/LogEntry.js
@@ -3,15 +3,26 @@ import React, { Component } from 'react';
 
 class LogEntry extends Component{
 
+  calculateEndTime=(start)=>{
+    const duration = this.props.insulinObj.insulin_duration_in_minutes
+    console.log(duration)
+  }
+
+
   render(){
     const {entryObj}=this.props
+    const datetime = new Date(entryObj.entry_date_and_time)
+    const formattedDate = datetime.toDateString()
+    const formattedStartTime = datetime.toLocaleTimeString('en-US')
+    const formattedEndTime = this.calculateEndTime(datetime)
+
     return(
       <tr className={entryObj.status === "active" ? "ui inverted green table segment" : null}>
-      <td data-label="date">{entryObj.entry_date_and_time}</td>
-      <td data-label="start-time">{entryObj.entry_date_and_time}</td>
-      <td data-label="end_time">{entryObj.end_time}</td>
-      <td data-label="time_left">{entryObj.time_left}</td>
-      <td data-label="status">{entryObj.status}</td>
+      <td data-label="date">{formattedDate}</td>
+      <td data-label="start-time">{formattedStartTime}</td>
+      <td data-label="end_time">end_time</td>
+      <td data-label="time_left">time left</td>
+      <td data-label="status">status</td>
       </tr>
     )
   }
--- src/Components/TimerCard.js
+++ src/Components/TimerCard.js
@@ -7,7 +7,7 @@ class TimerCard extends Component{
       <div className="ui center aligned green inverted segment">
 
       <h3>{this.props.insulinObj.insulin_name} timer</h3>
-      <h1> {this.props.entryObj.time_left} of {this.props.insulinObj.insulin_duration_in_minutes} remaining</h1>
+      <h1> *time left* of {this.props.insulinObj.insulin_duration_in_minutes} remaining</h1>
       <h3>Last Administered {this.props.entryObj.entry_date_and_time}</h3>
 
       </div>
